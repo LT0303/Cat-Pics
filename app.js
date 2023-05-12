1736372fb4816d5d84ef3111cbdd9040429bc331
L. Thackeray <lthackeray33@gmail.com>
2023-05-12 16:29:51 -0400

    Changed id of catPics button to getPic, updated app.js to reflect Created div to append cat pics to changed makePic function to append to said div

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
-const catPics = document.querySelector('#catPics');
+const catPics = document.querySelector('#getPic');
 
-catPics.addEventListener('submit', async function (e) {
+getPic.addEventListener('submit', async function (e) {
         e.preventDefault();
         const config = { header: { 'x-api-key': 'live_A8b2HZBkcv6PsexlEtpA5yDhBpUGjPuTQjqWfmJSu8mKUPyfp0IeawfRHMOOvlNN' } };
         const res = await axios.get(`https://api.thecatapi.com/v1/images/search`, config);
         console.log(res.data);
         makePic(res);
+
 })
 
 const makePic = (pic) => {
     const img = document.createElement('img');
     img.src = pic.data[0].url;
-    document.body.append(img);
+    document.body.children[2].append(img);
 }
