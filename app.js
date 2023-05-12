41067e1e27feee7b33b6e3236c5005edfe720cb9
L. Thackeray <lthackeray33@gmail.com>
2023-05-12 16:32:22 -0400

    Removed console.log from listener function

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ getPic.addEventListener('submit', async function (e) {
         e.preventDefault();
         const config = { header: { 'x-api-key': 'live_A8b2HZBkcv6PsexlEtpA5yDhBpUGjPuTQjqWfmJSu8mKUPyfp0IeawfRHMOOvlNN' } };
         const res = await axios.get(`https://api.thecatapi.com/v1/images/search`, config);
-        console.log(res.data);
         makePic(res);
 
 })
