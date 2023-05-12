48138605ecc9e75185ac5b03bb1b601c8df87383
L. Thackeray <lthackeray33@gmail.com>
2023-05-12 15:44:10 -0400

    Selected pics and GIFs button, prevented default submit behavior

---
+++ app.js
@@ -0,0 +1,10 @@
+const catPics = document.querySelector('#catPics');
+const catGIFs = document.querySelector('#catGIFs');
+
+catPics.addEventListener('submit', async function (e) {
+        e.preventDefault();
+    })
+
+catGIFs.addEventListener('submit', async function (e) {
+    e.preventDefault();
+})
