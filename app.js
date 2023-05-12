0bcd9fe02b05374076d01d6d1d3e004ff9ead166
L. Thackeray <lthackeray33@gmail.com>
2023-05-12 16:17:51 -0400

    Finished makePic function, added api key and header, finished basic listener function

--- app.js
+++ app.js
@@ -2,13 +2,14 @@ const catPics = document.querySelector('#catPics');
 
 catPics.addEventListener('submit', async function (e) {
         e.preventDefault();
-        const res = await axios.get(`https://api.thecatapi.com/v1/images/search`);
+        const config = { header: { 'x-api-key': 'live_A8b2HZBkcv6PsexlEtpA5yDhBpUGjPuTQjqWfmJSu8mKUPyfp0IeawfRHMOOvlNN' } };
+        const res = await axios.get(`https://api.thecatapi.com/v1/images/search`, config);
         console.log(res.data);
-        //makePic(res);
+        makePic(res);
 })
 
-function makePic(pic) {
+const makePic = (pic) => {
     const img = document.createElement('img');
-    img.src = pic.data;
+    img.src = pic.data[0].url;
     document.body.append(img);
 }
